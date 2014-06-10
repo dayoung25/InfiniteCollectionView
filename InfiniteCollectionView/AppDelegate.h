131413b69d6de3888970047f3cf96baefc511a38
Dayoung Jung <dayoung25@daumcorp.com>
2014-06-10 14:18:56 +0900

    Initial Commit

---
+++ InfiniteCollectionView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  InfiniteCollectionView
+//
+//  Created by Dayoung on 2014. 6. 10..
+//  Copyright (c) 2014년 Dayoung Jung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ InfiniteCollectionView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  InfiniteCollectionView
+//
+//  Created by Dayoung on 2014. 6. 10..
+//  Copyright (c) 2014년 Dayoung Jung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
